1c42ba9b4ce3ebb324f00504733de299c6864229
DUCMINHHHH <trinhducminh296@gmail.com>
2023-07-05 23:23:01 +0700

    bai 25

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,8 +7,39 @@ class MyComponent extends React.Component {
     age: 21,
   };
 
+  handleClick(event) {
+    console.log("my name is", this.state.name, " iam", this.state.age);
+
+    this.setState({
+      name: "dm",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  }
+
+  handleMouseOver(event) {
+    console.log(event);
+  }
+
   render() {
-    return <div>my name is {this.state.name}</div>;
+    return (
+      <div>
+        my name is {this.state.name} iam {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
+        <button
+          onMouseOver={(event) => {
+            this.handleMouseOver(event);
+          }}
+        >
+          hover me
+        </button>
+      </div>
+    );
   }
 }
 
