2c9c55bef8e10039a745d5f0e68745ce080c0119
DUCMINHHHH <trinhducminh296@gmail.com>
2023-07-06 15:49:13 +0700

    video30-showhidefeature

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,14 +1,41 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+
+  handleHideShow = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+
   render() {
-    console.log(this.props);
-    const { name, age } = this.props;
+    const { listUsers } = this.props;
+    console.log(listUsers);
     return (
       <div>
-        <div>my name is {name}</div>
-        <div>my age is {age}</div>
-        <div>{this.props.myInfor}</div>
+        <div>
+          <span onClick={() => this.handleHideShow()}>
+            {this.state.isShowListUser === true
+              ? "hide list users"
+              : "show list users"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div> my name is {user.name} </div>
+                  <div> my age is {user.age} </div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,13 +3,19 @@ import UserInfo from "./UserInfo";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "DM", age: "16" },
+      { id: 2, name: "DMinh", age: "25" },
+      { id: 3, name: "minh", age: "69" },
+    ],
+  };
   render() {
-    const myInfor = ["a", "b", "c"];
     return (
       <div>
         <UserInfo />
         <br></br>
-        <DisplayInfor name={"DM"} age={30} MyInfor={myInfor} />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
