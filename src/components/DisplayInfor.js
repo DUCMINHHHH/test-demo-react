b00294b94abbc7f0d394658b8713d6cd9b931ca7
DUCMINHHHH <trinhducminh296@gmail.com>
2023-07-07 14:46:34 +0700

    video40

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,74 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+// class DisplayInfor extends React.Component {
+//   render() {
+//     const { listUsers } = this.props;
+//     console.log(listUsers);
+//     return (
+//       <div className="display-infor-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div> my name is {user.name} </div>
+//                   <div> my age is {user.age} </div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidMount = () => {
-    setTimeout(() => {
-      // hàm thay tên title
-      document.title = "minhDM";
-    }, 3000);
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+  // giống hàm phía trên
+  // this.state = {
+  //   isShowHideListUser: true;
+  // }
+
+  const handleShowHide = () => {
+    setShowHideListUser(!isShowHideListUser);
   };
 
-  componentDidUpdate = (prevProps, prevState, Snapshot) => {
-    console.log("call me component", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("me");
-      }
-    }
-  };
-
-  handleHideShow = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-
-  render() {
-    const { listUsers } = this.props;
-    console.log(listUsers);
-    return (
-      <div className="display-infor-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span onClick={() => this.handleHideShow()}>
-            {/* điều kiện để showhide */}
-            {this.state.isShowListUser === true
-              ? "hide list users"
-              : "show list users"}
-          </span>
-        </div>
-        {this.state.isShowListUser && (
-          <>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div> my name is {user.name} </div>
-                  <div> my age is {user.age} </div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      delete
-                    </button>
-                  </div>
-                  <hr />
-                </div>
-              );
-            })}
-          </>
-        )}
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHide()}>
+          {isShowHideListUser === true ? "hide list user" : "show list user"}
+        </span>
       </div>
-    );
-  }
-}
+      {isShowHideListUser && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div> my name is {user.name} </div>
+                <div> my age is {user.age} </div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    delete
+                  </button>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
