35064418feb57e680ab6d361e97568ebd2018af8
DUCMINHHHH <trinhducminh296@gmail.com>
2023-07-11 11:09:07 +0700

    video52

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,32 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">home page component</div>;
+  return (
+    <div className="homepage-container">
+      <div className="homepage-video">
+        <video
+          poster="/static/homepage/hero/hero-poster.webp"
+          width="750"
+          height="500"
+          muted
+          loop
+          autoplay="autoplay"
+        >
+          <source
+            data-testid="currentWebmVideo"
+            src={videoHomepage}
+            type="video/webm"
+          />
+          <source
+            data-testid="currentDefaultVideo"
+            src={videoHomepage}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="homepage-content"></div>
+    </div>
+  );
 };
 
 export default HomePage;
