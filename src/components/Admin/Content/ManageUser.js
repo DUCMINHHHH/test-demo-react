a20174c4995bf2e7ac252b5a9f2fdd1e8a61287d
DUCMINHHHH <trinhducminh296@gmail.com>
2023-07-13 22:35:08 +0700

    video62

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -2,10 +2,25 @@ import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FaPlus } from "react-icons/fa";
 import TableUser from "./tableUser";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../services/apiServices";
 
 const ManageUser = (props) => {
   const [showModalCreateUser, setshowModalCreateUser] = useState(false);
+
+  const [listUser, setListUser] = useState([]);
+  // component didmount
+  useEffect(() => {
+    fetchListUser();
+  }, []);
+
+  const fetchListUser = async () => {
+    let res = await getAllUsers();
+    console.log(res);
+    if (res.EC === 0) {
+      setListUser(res.DT);
+    }
+  };
 
   return (
     <div className="manage-user-container">
@@ -20,11 +35,12 @@ const ManageUser = (props) => {
           </button>
         </div>
         <div className="table-users-container">
-          <TableUser />
+          <TableUser listUser={listUser} />
         </div>
         <ModalCreateUser
           show={showModalCreateUser}
           setShow={setshowModalCreateUser}
+          fetchListUser={fetchListUser}
         />
       </div>
     </div>
--- src/components/Admin/Content/tableUser.js
+++ src/components/Admin/Content/tableUser.js
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
-import { getAllUsers } from "../../../services/apiServices";
-
 const TableUser = (props) => {
-  const [listUser, setListUser] = useState([]);
-
-  useEffect(() => {
-    fetchListUser();
-  }, []);
-
-  const fetchListUser = async () => {
-    let res = await getAllUsers();
-    console.log(res);
-    if (res.EC === 0) {
-      setListUser(res.DT);
-    }
-  };
+  const { listUser } = props;
 
   return (
     <>
       <table className="table table-hover table-bordered">
         <thead>
           <tr>
-            <th scope="col">No</th>
+            <th scope="col">Id</th>
             <th scope="col">Username</th>
             <th scope="col">Email</th>
             <th scope="col">Role</th>
@@ -34,7 +19,7 @@ const TableUser = (props) => {
             listUser.map((item, index) => {
               return (
                 <tr key={`table-users-${index}`}>
-                  <td scope="row"> {index + 1}</td>
+                  <td scope="row"> {item.id}</td>
                   <td>{item.username}</td>
                   <td>{item.email}</td>
                   <td>{item.role}</td>
